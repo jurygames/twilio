1739a2b8331b8998a1879a8fe1b3d255da38db8c
jurygames <info@jurygames.com>
2025-06-19 09:21:07 +0100

    Update index.js

--- twilio-show-messenger/pages/index.js
+++ twilio-show-messenger/pages/index.js
@@ -1,8 +1,12 @@
+import GroupManager from '../components/GroupManager';
+import TemplateSelector from '../components/TemplateSelector';
+
 export default function Home() {
   return (
     <div style={{ padding: '2rem' }}>
       <h1>Twilio Show Messenger</h1>
-      <p>Welcome. Use the menu to select a show and start messaging.</p>
+      <TemplateSelector />
+      <GroupManager />
     </div>
   );
 }
