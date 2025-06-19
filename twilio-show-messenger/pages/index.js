0ddab44e5b417d35f57dfe4628c77b3982bc2f74
jurygames <info@jurygames.com>
2025-06-19 08:59:24 +0100

    Update index.js

--- twilio-show-messenger/pages/index.js
+++ twilio-show-messenger/pages/index.js
@@ -1,1 +1,9 @@
+export default function Home() {
+  return (
+    <div style={{ padding: '2rem' }}>
+      <h1>Twilio Show Messenger</h1>
+      <p>Select a show, choose a template, and send messages or calls.</p>
+    </div>
+  );
+}
 // Placeholder for pages/index.js
