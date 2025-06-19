4db77418d56f6ac338259a3ec83a1d28d6611917
jurygames <info@jurygames.com>
2025-06-19 09:25:52 +0100

    Update index.js

--- twilio-show-messenger/pages/index.js
+++ twilio-show-messenger/pages/index.js
@@ -1,12 +1,15 @@
+import { useState } from 'react';
 import GroupManager from '../components/GroupManager';
 import TemplateSelector from '../components/TemplateSelector';
 
 export default function Home() {
+  const [selectedTemplate, setSelectedTemplate] = useState(null);
+
   return (
     <div style={{ padding: '2rem' }}>
       <h1>Twilio Show Messenger</h1>
-      <TemplateSelector />
-      <GroupManager />
+      <TemplateSelector onSelect={setSelectedTemplate} />
+      <GroupManager template={selectedTemplate} />
     </div>
   );
 }
